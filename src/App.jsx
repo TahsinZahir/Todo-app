e5a6667f792f430e23d1394d561cf6e6ddbd7bfc
TahsinZahir <tahsindbg0142@gmail.com>
2024-12-14 00:55:02 +0530

    Added logical part of todo

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,21 @@
+import { useState } from 'react'
 import './App.css'
-
+import AddTodo from './components/AddTodo/AddTodo'
+import TodoList from './components/TodoList/TodoList'
 function App() {
-
+    const [todos,setTodos]= useState([
+      // {id:1,text: 'todo1',isFinite:true},
+      // {id:2,text: 'todo2',isFinite:false}
+    ]);
+    function addTodos(todoText){
+      let nextId=todos.length+1;
+    setTodos([...todos,{id:nextId, isFinished:false, text:todoText}]);
+    }
   return (
-    <>
-      Todos
-    </>
+  <>
+  <AddTodo addTodos={addTodos}/>
+  <TodoList todos={todos} setTodos={ setTodos }/>
+  </>
   )
 }
 
